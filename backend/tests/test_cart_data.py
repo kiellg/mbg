0de8ae5aa67c6ee9aa87fad8db34d04e3029ec8f
Yohanes Amelio Turnip <64017109+johanes147@users.noreply.github.com>
2026-03-04 11:51:24 -0800

    change import to from import to avoid using protected class

--- backend/tests/test_cart_data.py
+++ backend/tests/test_cart_data.py
@@ -1,7 +1,7 @@
 """Unit tests for cart_data simulated database structure."""
 
 from datetime import datetime
-import backend.app.data.cart_data as cart_data
+from backend.app.data import cart_data
 
 def setup_function():
     """Clear _CARTDB before each test to ensure a clean state."""
