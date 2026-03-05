00338eb1297046c2b7fe7a5ae4eb29d7bd172882
Ansella Kartono <ansellakartono05@gmail.com>
2026-03-05 00:29:01 -0800

    Add authentication service and schemas

---
+++ backend/app/schemas/auth.py
@@ -0,0 +1,14 @@
+"""Schemas for user account registration"""
+
+from pydantic import BaseModel, EmailStr
+from typing import Literal
+
+class RegisterRequest(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    role: Literal["customer", "manager", "driver"]
+
+class RegisterResponse(BaseModel):
+    user_id: int
+    email: EmailStr
---
+++ backend/app/services/auth_service.py
@@ -0,0 +1,32 @@
+import hashlib
+from fastapi import HTTPException
+
+from app.data.users_data import(
+    get_user_by_email,
+    create_user,
+    create_customer,
+    create_manager,
+    create_driver,
+)
+
+def hash_password(password: str) -> str:
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def register_user(name: str, email: str, password: str, role: str):
+    if get_user_by_email(email):
+        raise HTTPException(status_code=400, detail="Email already exists")
+    
+    password_hash = hash_password(password)
+
+    user = create_user(name, email, password_hash)
+
+    if role == "customer":
+        create_customer(user["user_id"])
+
+    elif role == "manager":
+        create_manager(user["user_id"])
+
+    elif role == "driver":
+        create_driver(user["user_id"])
+
+    return user
