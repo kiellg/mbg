45b0b86cc46f198d76963ca9b088b565206becec
Ansella Kartono <ansellakartono05@gmail.com>
2026-04-07 15:32:34 -0700

    Fix import order

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,17 @@
 """Main FastAPI app"""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:1573"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 from app.routers.auth import router as auth_router
 from app.routers.restaurants import router as restaurants_router
 from app.routers.carts import router as carts_router
@@ -16,16 +27,6 @@ from app.routers.reviews import router as reviews_router
 from app.routers.favourites import router as favourites_router
 from app.routers.coupons import router as coupons_router
 from app.routers.admin import router as admin_router
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:1573"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(auth_router)
 app.include_router(restaurants_router)
