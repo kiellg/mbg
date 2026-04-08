b012994b60a12fcdf09a3d5791f1c7a1894afdd1
Ansella Kartono <ansellakartono05@gmail.com>
2026-04-08 07:38:49 -0700

    fix pylint

--- backend/app/schemas/checkout.py
+++ backend/app/schemas/checkout.py
@@ -1,10 +1,10 @@
 """Schemas for checkout request payload."""
 
+from datetime import datetime
+
 from typing import Optional
 
 from pydantic import BaseModel
-
-from datetime import datetime
 
 from app.schemas.order import DeliveryMethod
 
